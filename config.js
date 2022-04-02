6925f5880652b2d9c13ddca3596d698f78c6bcac
Fermion12 <dieterhulshof@gmail.com>
2022-04-02 12:14:27 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const config = {
   style: 'mapbox://styles/mapbox/light-v10',
   accessToken:
     'pk.eyJ1IjoiZmVybWlvbiIsImEiOiJjbDFoam1uYXAwNThlM3JsZDc5d3BlbjdtIn0.VfsiH5WkSyECq--9hR4LVw',
-  CSV: './Hike_Data.csv',
+  CSV: './Sample_Data.csv',
   center: [21.28718367062586, -32.766440696658535],
   zoom: 6,
   title: 'Find Your Next Hike',
